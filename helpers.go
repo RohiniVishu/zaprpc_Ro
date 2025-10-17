571ff43bc9f0f4681c6e9263905806fff32bf0fc
Rohini Vishu <200266036+RohiniVishu@users.noreply.github.com>
2025-10-18 03:30:39 +0530

    latest

--- helpers.go
+++ helpers.go
@@ -6,62 +6,43 @@ import (
 	"github.com/quic-go/quic-go"
 	"io"
 	"net"
-	"go.uber.org/zap"
 )
 
-// isGracefulClose determines if an error represents a normal connection close
-func isGracefulClose(err error, logger *zap.Logger) bool {
+// isGracefulClose returns true if the error is considered a normal/expected close
+func isGracefulClose(err error) bool {
 	if err == nil {
-		logger.Debug("graceful close: nil error")
 		return true
 	}
 
 	switch {
 	case errors.Is(err, io.EOF):
-		logger.Debug("graceful close: EOF")
 		return true
 	case errors.Is(err, net.ErrClosed):
-		logger.Debug("graceful close: net.ErrClosed")
 		return true
 	case errors.Is(err, context.Canceled):
-		logger.Debug("graceful close: context canceled")
 		return true
 	}
 
 	var appErr *quic.ApplicationError
-	if errors.As(err, &appErr) {
-		logger.Debug("graceful close: QUIC application error", zap.String("error", appErr.Error()))
-		return true
-	}
-
-	logger.Debug("non-graceful close detected", zap.Error(err))
-	return false
+	return errors.As(err, &appErr)
 }
 
-// isTimeout determines if an error represents a timeout event
-func isTimeout(err error, logger *zap.Logger) bool {
+// isTimeout returns true if the error is a timeout
+func isTimeout(err error) bool {
 	if err == nil {
 		return false
 	}
 
 	if errors.Is(err, context.DeadlineExceeded) {
-		logger.Debug("timeout detected: context deadline exceeded")
 		return true
 	}
 
 	type timeoutIface interface{ Timeout() bool }
 	var te timeoutIface
 	if errors.As(err, &te) && te.Timeout() {
-		logger.Debug("timeout detected: implements Timeout()")
 		return true
 	}
 
 	var idle *quic.IdleTimeoutError
-	if errors.As(err, &idle) {
-		logger.Debug("timeout detected: QUIC idle timeout", zap.String("error", idle.Error()))
-		return true
-	}
-
-	logger.Debug("error is not a timeout", zap.Error(err))
-	return false
+	return errors.As(err, &idle)
 }
