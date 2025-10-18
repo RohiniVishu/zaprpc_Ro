9507899a351d45a7a29a68bad4512980ddaf0d0b
Rohini Vishu <200266036+RohiniVishu@users.noreply.github.com>
2025-10-18 06:19:26 +0530

    4_config

--- config.go
+++ config.go
@@ -1,24 +1,6 @@
-package zaprpc
-
 import (
 	"crypto/tls"
 
 	"github.com/quic-go/quic-go"
 	"go.uber.org/zap"
 )
-
-type ClientConfig struct {
-	Logger *zap.Logger
-	Codec  Codec
-}
-
-type ServerConfig struct {
-	Logger    *zap.Logger
-	Codec     Codec
-	TLSConfig *tls.Config
-}
-
-type ConnectionConfig struct {
-	TLSConfig  *tls.Config
-	QUICConfig *quic.Config
-}
