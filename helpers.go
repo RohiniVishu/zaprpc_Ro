048634baf8a5e3756bc240885c2d7bc411d33f4e
Rohini Vishu <200266036+RohiniVishu@users.noreply.github.com>
2025-10-18 06:37:21 +0530

    helper.go finalfinalfinal
    Refactor isGracefulClose to handle multiple error types and improve readability.

--- helpers.go
+++ helpers.go
@@ -8,41 +8,35 @@ import (
 	"net"
 )
 
-// isGracefulClose returns true if the error is considered a normal/expected close
 func isGracefulClose(err error) bool {
 	if err == nil {
 		return true
 	}
-
 	switch {
-	case errors.Is(err, io.EOF):
-		return true
-	case errors.Is(err, net.ErrClosed):
-		return true
-	case errors.Is(err, context.Canceled):
+	case errors.Is(err, io.EOF),
+		errors.Is(err, net.ErrClosed),
+		errors.Is(err, context.Canceled):
 		return true
 	}
-
 	var appErr *quic.ApplicationError
-	return errors.As(err, &appErr)
+	if errors.As(err, &appErr) {
+		return true
+	}
+	return false
 }
 
-// isTimeout returns true if the error is a timeout
 func isTimeout(err error) bool {
 	if err == nil {
 		return false
 	}
-
 	if errors.Is(err, context.DeadlineExceeded) {
 		return true
 	}
-
 	type timeoutIface interface{ Timeout() bool }
 	var te timeoutIface
 	if errors.As(err, &te) && te.Timeout() {
 		return true
 	}
-
 	var idle *quic.IdleTimeoutError
 	return errors.As(err, &idle)
 }
